d7611e2a71de5ab7e811fb6b2668fe78f4fe9361
Tervi test <Tervi@list.ru>
2023-04-14 13:23:40 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 189999; // стоимость билета
+        int y = 20; // стоимость бонуса
+        if (x > y);
+    int bonys = x / y;
+        System.out.println("Бонус за покупку равен:");System.out.println(bonys);
+}
+
+}
